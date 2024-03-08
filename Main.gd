4efcb1075539c708b1cfd2c57f59a44a1df2411a
root <100104632+Riizlaah@users.noreply.github.com>
2024-03-08 20:36:20 +0700

    update1

--- Main.gd
+++ Main.gd
@@ -1,20 +1,26 @@
 extends Control
 
 const MENU = preload("res://Menu.tscn")
-@onready var flow_container = $FlowContainer
+@onready var flow_container = $MCont/Vbox/SCont/FlowContainer
+@onready var label = $MCont/Vbox/Ip_List/Label
+
 
 func _ready():
 	_refresh()
+	print(str(OS.get_environment("HOSTNAME")))
 
 func _refresh():
 	var interfaces = IP.get_local_interfaces()
 	for node in flow_container.get_children():
 		if node.name != "Refresh": node.queue_free()
-	await get_tree().create_timer(0.5).timeout
-	for interface in interfaces:
+	label.text = ""
+	await get_tree().create_timer(0.25).timeout
+	for i in interfaces.size():
 		var menu = MENU.instantiate()
 		flow_container.add_child(menu)
-		menu.get_node("Scont/Vbox/Label").text = interface["friendly"] + " | " + interface["name"]
-		menu.get_node("Scont/Vbox/Label2").text = "|---------------------|\n" + str(interface["addresses"])
+		menu.get_node("Scont/Vbox/Hbox/Label").text = str(i) + " =>"
+		menu.get_node("Scont/Vbox/Hbox/Label2").text = interfaces[i]['friendly'] + "\n" + interfaces[i]["name"]
+		menu.get_node("Scont/Vbox/Label2").text = str(interfaces[i]["addresses"])
+	label.text = str(IP.get_local_addresses())
 
 
